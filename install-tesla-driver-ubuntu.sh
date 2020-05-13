a3fdba23fe4d74378ed1f52bfd37348f848055f0
andrasdanko <62292289+andrasdanko@users.noreply.github.com>
2020-05-13 18:47:49 +0200

    Create install-tesla-driver-ubuntu.sh
    init

---
+++ install-tesla-driver-ubuntu.sh
@@ -0,0 +1,10 @@
+# uname -m
+# lspci | grep NVIDIA (
+# https://www.nvidia.com/Download/driverResults.aspx/158190/en-us
+
+wget http://uk.download.nvidia.com/tesla/440.64.00/nvidia-driver-local-repo-ubuntu1604-440.64.00_1.0-1_amd64.deb
+sudo dpkg -i nvidia-driver-local-repo-ubuntu1604-440.64.00_1.0-1_amd64.deb
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+
+echo "Reboot required."
